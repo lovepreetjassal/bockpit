f3d64a45971eb4f0a3fe9f7d7841d333c35c7f5a
ice <jassalovepreet@gmail.com>
2024-04-21 19:22:50 -0400

    UI Update

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet,RouterLink, RouterLinkActive],
+  imports: [RouterOutlet,RouterLink, RouterLinkActive,MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
