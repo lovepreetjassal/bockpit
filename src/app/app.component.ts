b57b768b104d4e5ed8f25fd3ed08c77fbe8d3422
ice <jassalovepreet@gmail.com>
2024-04-21 18:48:10 -0400

    New Action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bockpit';
+
+  ngOnInit(){
+    console.log(`Init`)
+  }
 }
